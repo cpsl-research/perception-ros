8dbfb1ca233349c727c54d6e8b14b51c434a7d49
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2024-07-29 17:59:18 -0400

    Transform (#1)
    * WIP on getting proper detection coordinate frame
    
    * Fix height transform error in fov estimator
    
    * Change parameter names on percep model
    
    '
    
    ---------
    
    Co-authored-by: spencer@primus <spencer.hallyburton@duke.edu>

--- fov_estimator/fov_estimator/lidar_concave_hull.py
+++ fov_estimator/fov_estimator/lidar_concave_hull.py
@@ -1,10 +1,15 @@
 import rclpy
 from rclpy.node import Node
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 
 from geometry_msgs.msg import PolygonStamped
 from sensor_msgs.msg import PointCloud2 as LidarMsg
+from avstack_bridge import Bridge
 from avstack_bridge.geometry import GeometryBridge
 from avstack_bridge.sensors import LidarSensorBridge
+from avstack_bridge.transform import do_transform_cloud
 
 from avstack.modules.perception.fov_estimator import ConcaveHullLidarFOVEstimator
 
@@ -12,7 +17,7 @@ from avstack.modules.perception.fov_estimator import ConcaveHullLidarFOVEstimato
 class LidarFovEstimator(Node):
     def __init__(self):
         super().__init__("fov_estimator")
-        self.model = ConcaveHullLidarFOVEstimator()
+        self.model = ConcaveHullLidarFOVEstimator(max_height=5)
 
         qos = rclpy.qos.QoSProfile(
             history=rclpy.qos.QoSHistoryPolicy.KEEP_LAST,
@@ -20,6 +25,10 @@ class LidarFovEstimator(Node):
             reliability=rclpy.qos.QoSReliabilityPolicy.RELIABLE,
             durability=rclpy.qos.QoSDurabilityPolicy.VOLATILE
         )
+
+        # listen to transform information
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self, qos=qos)
 
         # subscribe to point cloud in the same namespace
         self.subscriber_pc = self.create_subscription(
@@ -37,7 +46,23 @@ class LidarFovEstimator(Node):
         )
 
     def pc_callback(self, pc_msg: LidarMsg) -> LidarMsg:
-        pc_avstack = LidarSensorBridge.pc2_to_avstack(pc_msg)
+        """Run the FOV estimation model when we receive lidar data
+        
+        We need to project the lidar data into either a ground or global
+        reference frame to handle perspective angle sensors
+        """
+        try:
+            tf_world_lidar = self.tf_buffer.lookup_transform(
+                "world",
+                pc_msg.header.frame_id,
+                pc_msg.header.stamp,
+            )
+        except TransformException as ex:
+            self.get_logger().info(
+                f'Could not transform point cloud for fov estimation')
+            return
+        pc_msg_global = do_transform_cloud(pc_msg, tf_world_lidar)
+        pc_avstack = LidarSensorBridge.pc2_to_avstack(pc_msg_global)
         fov_avstack = self.model(pc_avstack)
         fov_ros = GeometryBridge.avstack_to_polygon(fov_avstack, stamped=True)
         self.publisher_fov.publish(fov_ros)
--- percep_lidar/percep_lidar/mmdetection3d.py
+++ percep_lidar/percep_lidar/mmdetection3d.py
@@ -1,9 +1,13 @@
 import rclpy
 from rclpy.node import Node
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 
 from sensor_msgs.msg import PointCloud2 as LidarMsg
 from vision_msgs.msg import Detection3DArray
 
+from avstack_bridge import Bridge
 from avstack_bridge.sensors import LidarSensorBridge
 from avstack_bridge.detections import DetectionBridge
 
@@ -11,9 +15,20 @@ from avstack.modules.perception.object3d import MMDetObjectDetector3D
 
 
 class LidarPerception(Node):
-    def __init__(self):
-        super().__init__("perception")       
-        self.model = MMDetObjectDetector3D(model='pointpillars', dataset='carla-vehicle')
+    def __init__(self, verbose: bool = False):
+        super().__init__("perception")
+        self.declare_parameter("perception_model", "pointpillars")
+        self.declare_parameter("perception_dataset", "carla-vehicle")
+
+        # initialize models
+        param_model = self.get_parameter("perception_model").value
+        param_dataset = self.get_parameter("perception_dataset").value
+        self.model = MMDetObjectDetector3D(
+            model=param_model,
+            dataset=param_dataset,
+        )
+        self.get_logger().info(f"Initialized {param_model} model on {param_dataset} dataset")
+        self.verbose = verbose
 
         qos = rclpy.qos.QoSProfile(
             history=rclpy.qos.QoSHistoryPolicy.KEEP_LAST,
@@ -21,6 +36,10 @@ class LidarPerception(Node):
             reliability=rclpy.qos.QoSReliabilityPolicy.RELIABLE,
             durability=rclpy.qos.QoSDurabilityPolicy.VOLATILE
         )
+
+        # listen to transform information
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self, qos=qos)
 
         # subscribe to point cloud in the same namespace
         self.subscriber_pc = self.create_subscription(
@@ -38,8 +57,41 @@ class LidarPerception(Node):
         )
 
     def pc_callback(self, pc_msg: LidarMsg) -> Detection3DArray:
+        """Run perception model when we receive lidar data
+        
+        We need to obtain the actual reference frame for the lidar sensor
+        because the perception model runs detection on a nominal sensor
+        height. Therefore, we need to adjust the perception outcomes
+        based on the difference between the nominal height and the realized
+        sensor height.
+        """
+        # get the transformations
+        # HACK: assume the sensor is e.g., agent0/lidar0
+        try:
+            frame_agent = pc_msg.header.frame_id.split("/")[0]
+            tf_world_agent = self.tf_buffer.lookup_transform(
+                "world",
+                frame_agent,
+                pc_msg.header.stamp,
+            )
+            tf_agent_lidar = self.tf_buffer.lookup_transform(
+                frame_agent,
+                pc_msg.header.frame_id,
+                pc_msg.header.stamp,
+            )
+            ref_agent_world = Bridge.tf2_to_reference(tf_world_agent)
+            ref_lidar_agent = Bridge.tf2_to_reference(tf_agent_lidar, reference=ref_agent_world)
+        except TransformException as ex:
+            self.get_logger().info(
+                f'Could not transform point cloud for perception')
+            return
+
+        # run the pipeline
         pc_avstack = LidarSensorBridge.pc2_to_avstack(pc_msg)
+        pc_avstack.calibration.reference = ref_lidar_agent
         dets_avstack = self.model(pc_avstack)
+        if self.verbose:
+            self.get_logger().info(f"Lidar perception generated {len(dets_avstack)} detections")
         dets_ros = DetectionBridge.avstack_to_detectionarray(dets_avstack, header=pc_msg.header)
         self.publisher_dets.publish(dets_ros)
 
